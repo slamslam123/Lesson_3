374b889223175544a7f79ccff7d3053524c57432
Никита Киреев <slamslam123@vk.com>
2022-05-20 14:41:10 +0300

    Добавьте файлы проекта.

---
+++ Work2/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Work2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("BLACKJACK \nЗдравствуйте , введите количество карт :");
+            byte amountCards = byte.Parse(Console.ReadLine());
+            int sum = 0;
+            Console.WriteLine("Карты от 1 до 10 J=10 Q=10 K=10 T=10 ");
+            for (int i = 0; i < amountCards; i++)
+            {
+                Console.WriteLine("Введите номинал карты: ");
+                string nominal = (Console.ReadLine());
+                switch (nominal)
+                {
+                    case "J":
+                    case "Q":
+                    case "K":
+                    case "T":
+                        sum = sum + 10;
+                        break;
+                    case "1":
+                        sum = sum + 1;
+                        break;
+                    case "2":
+                        sum = sum + 2;
+                        break;
+                    case "3":
+                        sum = sum + 3;
+                        break;
+                    case "4":
+                        sum = sum + 4;
+                        break;
+                    case "5":
+                        sum = sum + 5;
+                        break;
+                    case "6":
+                        sum = sum + 6;
+                        break;
+                    case "7":
+                        sum = sum + 7;
+                        break;
+                    case "8":
+                        sum = sum + 8;
+                        break;
+                    case "9":
+                        sum = sum + 9;
+                        break;
+                    case "10":
+                        sum = sum + 10;
+                        break;
+                    default:
+                        i--;
+                        Console.WriteLine("Номинал карты введен не корректно");
+                        break;
+                }
+            }
+            Console.WriteLine($"Колличество карт в Вашей руке: {amountCards} \nНабранное колличество очков: {sum}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Work3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Work3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число:");
+            int numb = int.Parse(Console.ReadLine());
+            bool n = true;
+
+            while (true)
+            {
+
+            }
+        }
+    }
+}
